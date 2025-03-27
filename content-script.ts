ed95369847ec14a1a306c8f35cbf0fab5a6cafa0
om-dev-123 <om.dev@autofintech.co.uk>
2025-03-27 20:32:25 +0530

    process message in content-script instead of service-worker

--- content-script.ts
+++ content-script.ts
@@ -1,15 +1,80 @@
 import browser from "webextension-polyfill";
-import { TMessage } from "./type";
+import { TMessage, TResponse } from "./type";
 
-console.log("hello");
+const processMessage = async (message: any) => {
+  console.debug("svw received");
 
+  //TODO: validation usign zod? check zod size or do custom validation
+  const msg = message as TMessage;
+  if (msg.api === "LocalStorage") {
+    if (msg.action === "clear") {
+      await browser.storage.local.clear();
+      // browser.storage.local
+      const response: TResponse = {
+        api: "LocalStorage:Response",
+        action: "clear",
+        requestId: msg.requestId,
+      };
+      return response;
+    }
+
+    // TODO: make it(type) narrow such that api should be same? Might be invalid issue
+    if (msg.action === "getItem") {
+      const result = await browser.storage.local.get(msg.key);
+      const response: TResponse = {
+        api: "LocalStorage:Response",
+        action: "getItem",
+        result: result[msg.key] ?? null,
+        requestId: msg.requestId,
+      };
+
+      return response;
+    }
+
+    if (msg.action === "removeItem") {
+      await browser.storage.local.remove(msg.key);
+      const response: TResponse = {
+        api: "LocalStorage:Response",
+        action: "removeItem",
+        requestId: msg.requestId,
+      };
+      return response;
+    }
+    if (msg.action === "setItem") {
+      //TODO: some storage local promise might not fail
+      await browser.storage.local.set({ [msg.key]: msg.value });
+
+      const response: TResponse = {
+        api: "LocalStorage:Response",
+        action: "setItem",
+        requestId: msg.requestId,
+      };
+      return response;
+    }
+
+    if (msg.action === "clear") {
+      //TODO: some storage local promise might not fail
+      await browser.storage.local.clear();
+      const response: TResponse = {
+        api: "LocalStorage:Response",
+        action: "clear",
+        requestId: msg.requestId,
+      };
+      return response;
+    }
+  }
+  //TODO add domain check
+  //TODO xss
+  //TODO
+  // return true;
+};
 //TODO type
 window.addEventListener("message", async (e) => {
   console.log("got msg", e.data);
   const msg = e.data as TMessage;
   if (msg.api === "LocalStorage") {
     //TODO:validate
-    const res = await browser.runtime.sendMessage(msg);
+    const res = await processMessage(msg);
     window.postMessage(res);
   }
 });
