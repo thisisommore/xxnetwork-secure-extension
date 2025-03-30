3c048604c1c03ce4d5cefffc3734b54bf5b62c52
om-dev-123 <om.dev@autofintech.co.uk>
2025-03-30 17:34:17 +0530

    add home and import-keys route

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import Root from "./routes/+page.svelte";
-
+  import Home from "./routes/home/+page.svelte";
   // --- Hash Router Implementation ---
   // Initialize with the current route or default to "home" if no hash is provided.
   let currentRoute: string = $state(window.location.hash.slice(1) || "");
@@ -23,10 +23,7 @@
     {#if currentRoute === ""}
       <Root />
     {:else if currentRoute === "home"}
-      <section>
-        <h2>Home</h2>
-        <p>Welcome to the Home Page!</p>
-      </section>
+     <Home />
     {:else if currentRoute === "about"}
       <section>
         <h2>About</h2>
