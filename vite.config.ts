76952a554a575c2bc86f9e6aab1a9f6160d71266
Mf <38403795+DeBelg@users.noreply.github.com>
2025-03-30 21:35:28 +0200

    updated buildfile and rebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import webExtension from "vite-plugin-web-extension";
+import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    svelte(), 
-    webExtension(),
+    svelte(),
+    webExtension({
+      manifest: "./manifest.json",
+      assets: "public",
+      webExtConfig: {
+        background: {
+          entry: "service-worker.ts",
+        },
+        content: {
+          scripts: {
+            entries: {
+              contentScript: "src/content-script.ts",
+            },
+          },
+        },
+      },
+    }),
   ],
-});
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        "service-worker": "service-worker.ts",
+      },
+    },
+  },
+})
