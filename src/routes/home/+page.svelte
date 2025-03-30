fac14cbb290ce69e400d4f66a1c52e3986e8bead
thisisommore <ommore501@gmail.com>
2025-03-30 17:34:17 +0530

    add home and import-keys route

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,26 +1,8 @@
-<script>
-  // Function to clear local storage when the text is clicked
-  function clearLocalStorage() {
-    alert('Not implemented');
-  }
-</script>
-
-<main>
-  <p on:click={clearLocalStorage}>Clear LS</p>
-</main>
-
 <style>
-  p {
-    cursor: pointer;
-    color: #0066cc;
-    text-decoration: underline;
-  }
-  
-  p:hover {
-    color: #004080;
-  }
-  
-  main {
-    padding: 1rem;
-  }
-</style> 
+</style>
+<div>
+    <p>Import Keys</p>
+    <p>Export Keys</p>
+    <p>Clear Keys</p>
+</div>
+
