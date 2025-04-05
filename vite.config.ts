ca25342f32f5110244f4ac6a5d697e4bdb29cb3f
om-dev-123 <om.dev@autofintech.co.uk>
2025-04-05 13:02:27 +0530

    add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import webExtension from "vite-plugin-web-extension";
 import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), webExtension()],
